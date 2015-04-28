ecdfe3cfeb128cb2d8aaec716156721acae5b18b
Kenjiro Cho <kjc@iijlab.net>
2015-04-28 15:52:59 +0900

    add fonts

--- js/myagurim.js
+++ js/myagurim.js
@@ -1,6 +1,6 @@
 // configuration variables
-var cgi_path = "/cgi-bin/";	// path of the cgi-bin directory
-var timeoffset = 9;			// time offset
+var cgi_path = "cgi-bin/";	// path to the cgi-bin directory
+var timeoffset = 9;		// time offset
 // end of configuration variables
 
 var html = {
