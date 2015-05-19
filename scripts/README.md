6d86bdfb776b66541b93f49479fc26dfc1cff93b
Kenjiro Cho <kjc@iijlab.net>
2015-05-19 10:58:01 +0900

    introduce a new re-aggregation script

---
+++ scripts/README.md
@@ -0,0 +1,70 @@
+# reaggregate.sh
+========
+A script for reaggregating aguri2 traffic summaries.
+
+reaggregate.sh is used to re-aggregate traffic summaries created by
+aguri2.
+This script is supposed to be invoked by a cron job every hour.
+The script re-aggregate day's summary with 5-minute resolution.
+If the hour is 23 (11p.m.), the script also updates the monthly
+summary with 2-hour resolution and the yearly summary with 24-hour
+resolution.
+
+If the '-t' option is not specified, it aggregates day's log using
+the time: 1 hour before the current time.
+
+
+## Usage
+
+To re-aggregate today's dataset under a specified log directory:
+
+	reaggregate.sh [-d logdir]
+
+To copy today's data from a remote site and reaggregate under a
+specified log directory:
+
+	reaggregate.sh [-d logdir] [-r host:path] [-o owner]
+
+To re-aggregate a specific day:
+
+	reaggregate.sh [-d logdir] [-t yyyymmddHH]
+
+  + `-d logdir`:
+    Specify the log directory.  The re-aggregated files are created under
+    this directory.
+  
+  + `-r host:path`:  
+    Copy data from a remote site using rsync(1).  'host:path' is
+    passed to rsync(1) as remote directory
+    (e.g., 'host.example.com:/export/aguri2').
+
+  + `-o owner`:  
+    Specify the owner of new files when copying from a remote site.
+    'owner' is passed to '--chown=${owner}' for rsync(1).
+
+  + `-t yyyymmdd[HH]`:  
+    Specify the day to re-aggregate.  If 'HH' is '23', agurify2.sh
+    updates the monthly summary and the yearly summary.
+    Otherwise, agurify2.sh updates only the daily summary.
+
+## Examples
+
+To run reaggregate.sh at minute 1 every hour,
+set the following entry in the crontab.
+
+	1 * * * * /script_path/reaggregate.sh -d /export/aguri2 2>&1
+
+Similary, for copying the data from a remote host, and then, re-aggregate:
+
+	1 * * * * /script_path/reaggregate.sh -r 'host.example.com:/export/aguri2' -o 'kjc:staff' -d /export/aguri2 2>&1
+
+To update the daily summary on May 15, 2015:
+
+	reaggregate.sh -d /export/aguri2 -t 20150515
+
+Similarly, to update the day summary along with the monthly summary
+and yearly summary, add '23' hour to the time string.
+
+	reaggregate.sh -d /export/aguri2 -t 2015051523
+
+
---
+++ scripts/reaggregate.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#
+# usage:
+#  reaggregate.sh [-d logdir] # for re-aggregation by cron job
+#  reaggregate.sh [-d logdir] [-r host:path] [-o owner] # rsync/re-agg by cron
+#  reaggregate.sh [-d logdir] [-t YYYYmmddHH] # re-aggregate for a given day
+#
+
+logdir="/work/aguri2"	# log directory
+agurim="/usr/local/bin/agurim"	# agurim program
+timestamp=""	# timestamp in "yyyymmddHH".  specify 23 for HH to
+		# update monthly and yearly files
+
+# for remote copy
+rsync="/usr/local/bin/rsync"	# rsync program
+remote=""			# remote dir e.g., "agurim.example.com:/logdir"
+owner=""			# owner of logs e.g., "kjc:staff"
+
+# process arguments
+while getopts "d:o:p:r:s:t:" opt; do
+    case $opt in
+	"d" ) logdir="$OPTARG" ;;
+	"o" ) owner="$OPTARG" ;;
+	"r" ) remote="$OPTARG" ;;
+	"t" ) timestamp="$OPTARG" ;;
+	* ) echo "Usage: reaggregate.sh [-d logdir] [-r host:path] [-o owner] [-t yyyymmddHH]" 1>&2
+	    exit 1 ;;
+    esac
+done
+
+
+if [ "X${timestamp}" = "X" ]; then
+    # if timestamp is not specified, use 'current time' - '1 hour'
+    timestamp=$(/bin/date -v -1H '+%Y %m %d %H')
+    set -- $timestamp
+    year=$1
+    month=$2
+    day=$3
+    hour=$4
+    if [ "${min}" = "00" ]; then
+	# if zero minute, add extra wait for the primary aggregatin to finish
+	sleep 20
+    fi
+else
+    # extract date components from the time string
+    year=$(echo $timestamp | awk '{print substr($0, 1, 4)}')
+    month=$(echo $timestamp | awk '{print substr($0, 5, 2)}')
+    day=$(echo $timestamp | awk '{print substr($0, 7, 2)}')
+    hour=$(echo $timestamp | awk '{print substr($0, 9, 2)}')
+fi
+
+if [ "X${remote}" != "X" ]; then
+    # if remote host is specified, rsync files under the day directory
+    optown=""
+    if [ "X${owner}" != "X" ]; then
+	optown="--chown=${owner}"
+    fi
+    mkdir -p "${logdir}/${year}${month}"
+    mkdir -p "${logdir}/${year}${month}/${year}${month}${day}"
+    cmd="${rsync} -Cax ${optown} ${remote}/${year}${month}/${year}${month}${day}/ ${logdir}/${year}${month}/${year}${month}${day}"
+    eval echo "exec cmd: ${cmd}" 1>&2
+    eval "${cmd}"
+else
+    # sanity check
+    if [ ! -d "${logdir}/${year}${month}/${year}${month}${day}" ]; then
+	echo "log dir: ${logdir} does not exist" 1>&2
+	exit 1
+    fi
+fi
+
+#
+# update daily file (and monthly and yearly files)
+#
+cd "${logdir}/${year}${month}/${year}${month}${day}" || exit $?
+dstfile="${year}${month}${day}.agr"
+res="300" # time resolution (5 minutes)
+files="${year}${month}${day}.??????.agr"
+
+cmd="${agurim} -i ${res} ${files} > ${dstfile}"
+echo "exec cmd: ${cmd}" 1>&2
+eval "${cmd}"
+
+# if this is 11pm, update monthly and yearly files
+if [ "$hour" = "23" ]; then
+    res="7200" # time resolution (2 hours)
+    cd "${logdir}/${year}${month}"
+    dstfile="${year}${month}.agr"
+    files="${year}${month}??/${year}${month}??.agr"
+    cmd="${agurim} -i ${res} ${files} > ${dstfile}"
+    echo "exec cmd: ${cmd}" 1>&2
+    eval "${cmd}"
+
+    # also update yearly data
+    res="86400" # time resolution (24 hours)
+    cd "${logdir}"
+    dstfile="${year}.agr"
+    files="${year}??/${year}??.agr"
+    cmd="${agurim} -i ${res} ${files} > ${dstfile}"
+    echo "exec cmd: ${cmd}" 1>&2
+    eval "${cmd}"
+fi
+
+exit 0
